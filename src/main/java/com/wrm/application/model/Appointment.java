7c2abae0f87130d3e7ac0281ed64075b10d01ec8
VuDuc <ducvmhe160798@fpt.edu.vn>
2024-11-07 18:46:10 +0700

    test push

--- src/main/java/com/wrm/application/model/Appointment.java
+++ src/main/java/com/wrm/application/model/Appointment.java
@@ -15,6 +15,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class Appointment extends BaseModel{
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
