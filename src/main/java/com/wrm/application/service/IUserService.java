e51edf27e2e4943fe261abda9a5acc2d09bc6d91
VuDuc <ducvmhe160798@fpt.edu.vn>
2024-11-09 08:29:01 +0700

    add id to UserDTO and add getManagerHaveNotWarehouse api

--- src/main/java/com/wrm/application/service/IUserService.java
+++ src/main/java/com/wrm/application/service/IUserService.java
@@ -7,13 +7,15 @@ import com.wrm.application.model.User;
 import com.wrm.application.response.user.UserResponse;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface IUserService {
     UserResponse createUser(UserDTO userDTO) throws Exception;
 
     String login(String email, String password) throws Exception;
 
     User getUserByEmail(String email) throws Exception;
-
+    List<UserDTO> getManagerHaveNotWarehouse();
     void changePassword(String email, ChangePasswordDTO changePasswordDTO) throws Exception;
 
     void resetPassword(Long userId, String newPassword) throws Exception;
